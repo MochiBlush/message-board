be3600daa140c6ab77655d0bbc1bb34dadbb2f7c
Mochi <alyssamc17@gmail.com>
2023-03-13 12:23:04 -0400

    updated

--- profiles/models.py
+++ profiles/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from sorl.thumbnail import ImageField
 
+
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
@@ -12,12 +13,12 @@ class Profile(models.Model):
     )
     image = ImageField(upload_to='profiles')
 
-
     def __str__(self):
         return self.user.username
-    
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    """Create a new Profile object when Django is created"""
+    """Create a new Profile() object when a Django User is created."""
     if created:
         Profile.objects.create(user=instance)
--- til/urls.py
+++ til/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from feed import urls as feed_urls
 from django.conf import settings
 from django.conf.urls.static import static 
+from profiles import urls as profiles_urls
 
 
 
@@ -26,5 +27,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include(feed_urls, namespace="feed")),
+    path("profile/", include(profiles_urls, namespace="profiles")),
     path("", include("allauth.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
